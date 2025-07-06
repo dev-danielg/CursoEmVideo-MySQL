8a6abb9fb3adf00edbc727d8222d186ef3697089
Daniel Gonçalves <83424429+dev-danielg@users.noreply.github.com>
2025-07-06 13:09:10 -0300

    Update teste7.sql

--- testes/teste7.sql
+++ testes/teste7.sql
@@ -115,12 +115,16 @@ group by carga;
 # Agrupando e agregando
 select carga, count(nome) from cursos
 group by carga;
+
 select carga, count(*) from cursos where totaulas = 30
 group by carga;
+
 select carga, count(*) from cursos
 group by carga having count(*) > 3;
+
 select ano, count(*) from cursos where totaulas > 20
 group by ano having ano > 2012
 order by count(*) desc;
+
 select carga, count(*) from cursos where ano > 2015
 group by carga having carga > (select avg(carga) from cursos);
